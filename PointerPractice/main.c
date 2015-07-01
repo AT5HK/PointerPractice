c320110680a405d543917058924884e28221cd4f
Auston <auston.salvana@live.com>
2015-07-01 10:03:05 -0700

    finish save

--- PointerPractice/main.c
+++ PointerPractice/main.c
@@ -8,8 +8,23 @@
 
 #include <stdio.h>
 
+int a =13,b = 5,c = 10;
+
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    
+    
+    int *pntr1 = &a,*pntr2 = &b,*pntr3 = &c,*pntr4 = &c;
+    
+    *pntr1 *= 2;
+    *pntr3 += 1;
+    pntr4 = pntr2;
+    *pntr4 += 4;
+    
+    printf("%i\n", *pntr1);
+    printf("%d\n", c);
+    printf("%d", *pntr4);
+    
     return 0;
 }
